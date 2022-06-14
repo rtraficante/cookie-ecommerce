20f220dd1809c03ba81843cca00474bb69627e5d
RTraficante <rtraficante22@gmail.com>
2022-06-14 16:35:08 -0400

    added form to add products on frontend

--- client/Routes.js
+++ client/Routes.js
@@ -4,6 +4,7 @@ import { withRouter, Route, Switch, Redirect } from "react-router-dom";
 import { Login, Signup } from "./components/AuthForm";
 import Home from "./components/Home";
 import AllProducts from "./components/AllProducts";
+import AddProduct from "./components/AddProduct";
 import { me } from "./store";
 
 /**
@@ -24,6 +25,7 @@ class Routes extends Component {
             <Route path="/home" component={Home} />
             <Redirect to="/home" />
             <Route path="/products" component={AllProducts} />
+            <Route path="/add-product" component={AddProduct} />
           </Switch>
         ) : (
           <Switch>
@@ -31,6 +33,7 @@ class Routes extends Component {
             <Route path="/login" component={Login} />
             <Route path="/signup" component={Signup} />
             <Route path="/products" component={AllProducts} />
+            <Route path="/add-product" component={AddProduct} />
           </Switch>
         )}
       </div>
--- client/components/AddProduct.js
+++ client/components/AddProduct.js
@@ -1,16 +1,55 @@
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { addProduct } from "../store/allProducts";
 
-export const addProduct = (product) => {
+export const AddProduct = () => {
   const dispatch = useDispatch();
+
+  const [values, setValues] = useState({
+    name: "",
+    price: 0,
+    inventory: 0,
+  });
+
+  const handleChange = (event) => {
+    event.persist();
+    setValues((values) => ({
+      ...values,
+      [event.target.name]: event.target.value,
+    }));
+  };
 
   return (
     <form
-      onSubmit={() => {
-        dispatch(addProduct);
+      onSubmit={(event) => {
+        event.preventDefault();
+        dispatch(addProduct(values));
       }}
-    ></form>
+    >
+      <input
+        name="name"
+        type="text"
+        placeholder="Name"
+        value={values.name}
+        onChange={handleChange}
+      />
+      <input
+        name="price"
+        type="number"
+        placeholder="Price"
+        value={values.price}
+        onChange={handleChange}
+      />
+      <input
+        name="inventory"
+        type="number"
+        placeholder="Inventory"
+        value={values.inventory}
+        onChange={handleChange}
+      />
+      <button type="submit">Submit</button>
+    </form>
   );
 };
 
-export default addProduct;
+export default AddProduct;
