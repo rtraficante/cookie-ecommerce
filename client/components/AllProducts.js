10a9e193b2af5da318fc11fdbd3dc345a396ddfd
Ed Geng <egeng01@gmail.com>
2022-06-15 17:06:57 -0400

    completed singleProduct view and route

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -1,7 +1,7 @@
-import React, { useEffect } from "react";
-import { fetchProducts } from "../store/allProducts";
-import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import React, { useEffect } from 'react';
+import { fetchProducts } from '../store/allProducts';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 function AllProducts() {
   const products = useSelector((state) => state.allProducts);
@@ -15,7 +15,7 @@ function AllProducts() {
   return (
     <div>
       {products.map((product) => (
-        <Link to={`/product/${product.id}`} key={product.id}>
+        <Link to={`/products/${product.id}`} key={product.id}>
           <img src={product.imageURL} alt="image of cookie" />
           <h4>{product.name}</h4>
           <p>{product.price}</p>
---
+++ client/store/singleProduct.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+const FETCH_PRODUCT = 'FETCH_PRODUCT';
+
+export const _fetchProduct = (product) => ({
+  type: FETCH_PRODUCT,
+  product,
+});
+
+export const fetchProduct = (id) => {
+  return async (dispatch) => {
+    try {
+      const { data } = await axios.get(`/api/products/${id}`);
+      dispatch(_fetchProduct(data));
+    } catch (err) {
+      console.error(err);
+    }
+  };
+};
+
+const initalState = {};
+
+export const productReducder = (state = initalState, action) => {
+  switch (action.type) {
+    case FETCH_PRODUCT:
+      return action.product;
+    default:
+      return state;
+  }
+};
