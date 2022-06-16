a4759718be44ef6b829a6e6569ca709f7e32d422
Tim Brandau <96590473+tacticalbison@users.noreply.github.com>
2022-06-16 14:41:40 -0500

    Optimized routing and started admin components

--- client/components/AdminOverview.js
+++ client/components/AdminOverview.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const AdminOverview = () => {
-  return <div>Admin Overview</div>;
+  return (
+    <div>
+      Admin Overview
+      <Link to="/admin/products/add">Add Product</Link>
+    </div>
+  );
 };
 
 export default AdminOverview;
