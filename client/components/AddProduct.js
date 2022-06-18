710cdafc4375ca4bef615e468a1dd43f9616deb1
Tim Brandau <96590473+tacticalbison@users.noreply.github.com>
2022-06-17 19:20:12 -0500

    completed update product functionality

--- client/components/AddProduct.js
+++ client/components/AddProduct.js
@@ -10,8 +10,8 @@ export const AddProduct = () => {
     name: "",
     price: 0,
     inventory: 0,
-    imageURL: "",
     description: "",
+    imageURL: "",
   });
 
   const handleChange = (event) => {
@@ -24,46 +24,64 @@ export const AddProduct = () => {
 
   return (
     <form
+      id="add-product-form"
       onSubmit={(event) => {
         event.preventDefault();
+        if (values.imageURL === "") values.imageURL = undefined;
         dispatch(addProduct(values));
       }}
     >
-      <input
-        name="name"
-        type="text"
-        placeholder="Name"
-        value={values.name}
-        onChange={handleChange}
-      />
-      <input
-        name="price"
-        type="number"
-        placeholder="Price"
-        value={values.price}
-        onChange={handleChange}
-      />
-      <input
-        name="inventory"
-        type="number"
-        placeholder="Inventory"
-        value={values.inventory}
-        onChange={handleChange}
-      />
-      <input
-        name="description"
-        type="text"
-        placeholder="Description"
-        value={values.description}
-        onChange={handleChange}
-      />
-      <input
-        name="imageURL"
-        type="text"
-        placeholder="Image URL"
-        value={values.imageURL}
-        onChange={handleChange}
-      />
+      <label>
+        Name:
+        <input
+          name="name"
+          type="text"
+          placeholder="Name"
+          value={values.name}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Price:
+        <input
+          name="price"
+          type="number"
+          placeholder="Price"
+          value={values.price}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Inventory:
+        <input
+          name="inventory"
+          type="number"
+          placeholder="Inventory"
+          value={values.inventory}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Description:
+        <input
+          name="description"
+          type="text"
+          placeholder="Description"
+          value={values.description}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Image URL:
+        <input
+          name="imageURL"
+          type="text"
+          placeholder="Image URL"
+          value={values.imageURL}
+          onChange={handleChange}
+        />
+      </label>
+      <p></p>
       <Button variant="contained" color="primary" type="submit">
         Submit
       </Button>
