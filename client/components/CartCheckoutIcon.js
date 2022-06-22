9a120e5541e17dd8ac4eb0f183f1dee5c03c253a
Ed Geng <egeng01@gmail.com>
2022-06-22 00:04:19 -0400

    removed unused code

--- client/components/CartCheckoutIcon.js
+++ client/components/CartCheckoutIcon.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import Badge from "@material-ui/core/Badge";
 import { withStyles } from "@material-ui/core/styles";
 import IconButton from "@material-ui/core/IconButton";
