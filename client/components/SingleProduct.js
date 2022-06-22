34c7a4fb86f88b37a256ef4b27df41a51485f019
Ed Geng <egeng01@gmail.com>
2022-06-21 23:11:24 -0400

    new branch, styled single product component

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -3,6 +3,11 @@ import { fetchProduct } from "../store/singleProduct";
 import { addToCart } from "../store/cart";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
+import { Button, Typography, Card, CardMedia, CardContent, CardHeader, CardActions, Tooltip, IconButton, CssBaseline, Container } from "@material-ui/core";
+import { useProductStyles } from "../theme";
+import ShoppingCartOutlinedIcon from "@material-ui/icons/ShoppingCartOutlined";
+import RemoveIcon from "@material-ui/icons/Remove";
+import AddIcon from "@material-ui/icons/Add";
 
 function SingleProduct(props) {
   const history = useHistory();
@@ -18,28 +23,42 @@ function SingleProduct(props) {
 
   const handleAddToCart = () => {
     dispatch(addToCart(product, qty));
-    history.push("/cart");
+    history.push("/products");
   };
 
+  const addOne = () => setQty(qty++);
+  const minusOne = () => setQty(qty--);
   const handleQtyChange = (event) => {
     setQty(event.target.value);
   };
 
+  const classes = useProductStyles();
   const { imageURL, name, price, description } = product;
   return (
-    <>
-      <img src={imageURL} alt="Image of cookie" />
-      <h4>{name}</h4>
-      <p>{price}</p>
-      <p>Description: {description}</p>
-      <select value={qty} onChange={handleQtyChange}>
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-      </select>
-      <button onClick={handleAddToCart}>Add To Cart</button>
-    </>
+    <Container maxWidth="lg">
+      <Card xs={12} md={6} lg={3} elevation={3} style={{ display: "flex", flexDirection: "column", justifyContent: "space-evenly", height: "auto", background: "linear-gradient(to right top, pink, white, orange)" }}>
+        <CardHeader align="center" title={<Typography className={classes.h4}>{name}</Typography>} />
+        <CardMedia component="img" alt="img of cookie" image={imageURL} title={name} className={classes.media} />
+        <CardContent>
+          <Typography className={classes.p}>{description}</Typography>
+          <Typography className={classes.p}>${price}</Typography>
+        </CardContent>
+        <CardActions>
+          <Tooltip title="Add to cart">
+            <IconButton aria-label="Add to cart" onClick={handleAddToCart}>
+              <ShoppingCartOutlinedIcon />
+            </IconButton>
+          </Tooltip>
+          <Tooltip title="Remove">
+            <RemoveIcon fontSize="medium" onClick={minusOne} />
+          </Tooltip>
+          {qty}
+          <Tooltip title="Add">
+            <AddIcon fontSize="medium" onClick={addOne} />
+          </Tooltip>
+        </CardActions>
+      </Card>
+    </Container>
   );
 }
 
