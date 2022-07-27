1e757976ba12f6ea07f61df6b5f0d6fb5593ae05
RTraficante <rtraficante22@gmail.com>
2022-07-27 15:43:19 -0400

    messing around with tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx, html, css}"],
+  content: [
+    "./public/**/*.{js,jsx,ts,tsx, html, css}",
+    "./client/**/*.{js,jsx,ts,tsx, html, css}",
+    "./components/**/*.{js,jsx,ts,tsx, html, css}",
+    "./public/index.html",
+    "./public/bundle.js",
+  ],
   theme: {
     extend: {},
   },
