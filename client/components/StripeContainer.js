21a57b2f85079fec9cb179fae702a298ae6ef7f2
RTraficante <rtraficante22@gmail.com>
2022-07-27 16:21:05 -0400

    stripe env stuff

--- client/components/StripeContainer.js
+++ client/components/StripeContainer.js
@@ -3,9 +3,7 @@ import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 import PaymentForm from "./PaymentForm";
 
-const stripeTestPromise = loadStripe(
-  "pk_test_51LDBLhL5DnX6BuqIXxswtW44aAg0Ux6jif6SsWDTNCGWoh9cdzzzDa5WeY3sQF0bxBZWMBKMvfylw2CWeISpnZuY00SFWXhvlO"
-);
+const stripeTestPromise = loadStripe(process.env.STRIPE_PROMISE);
 
 const StripeContainer = () => {
   return (
